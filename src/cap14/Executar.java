6237675ffd72f8a3dd74868722fc16cbebf9c556
Alex Mota <alexmbcdo@gmail.com>
2022-08-29 22:06:22 -0300

    add cap 14 interface Funcional

--- src/cap14/Executar.java
+++ src/cap14/Executar.java
@@ -2,6 +2,7 @@ package cap14;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.*;
 
 public class Executar {
 
@@ -20,9 +21,14 @@ public class Executar {
         System.out.println("subtração: " + subtracao.executar(10, 30));
 
         OperacaoAritimetica funcionalSoma =
-                (parametro1 , parametro2) ->  parametro1 + parametro2;
+                (parametro1, parametro2) -> parametro1 + parametro2;
+
+        OperacaoAritimetica funcionalSomaReferencia = Double::sum;
+
+        System.out.println(funcionalSomaReferencia.executar(10.0, 30));
 
         ExibirDados interfaceExibir = s -> System.out.println(s);
+        ExibirDados interfaceExibirReferencia = System.out::println;
 
 
         double resultado = funcionalSoma.executar(3, 3);
@@ -34,14 +40,34 @@ public class Executar {
 
 
         ExibirDados interfaceExibir1 = s -> {
-            if (s > 10 ) {
+            if (s > 10) {
                 System.out.println(s);
-            }else {
+            } else {
                 System.out.println("valor menor que 10");
             }
         };
 
         interfaceExibir1.exibir(resultado);
 
+        Produto produto = new Produto("Sabonete", 2);
+        OperacaoAritimetica calculaNovoValor = produto::aumentarPreco;
+
+        OperacaoAritimetica calculaNovoValor1 = Produto::aumentarPreco1;
+
+        double valor1 = calculaNovoValor.executar(30.3, 20);
+        double valor2 = calculaNovoValor1.executar1(10, 10);
+
+        // classe Funcitonal Java 8
+        Consumer<Double> exibirValor = System.out::println;
+        exibirValor.accept(valor1);
+        exibirValor.accept(valor2);
+
+        BiFunction<Double, Double, Double> somar = Double::sum;
+        System.out.println(somar.apply(100.0, 30.3));
+        BiFunction<Double, Double, String> somar2 = (x,z) -> String.valueOf(x + z);
+        System.out.println(somar2.apply(60.0, 10.0));
+
+
     }
+
 }
