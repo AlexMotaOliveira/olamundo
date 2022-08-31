733c0f984e974fad76b2a09832718f990ada2df9
Alex Mota <alexmbcdo@gmail.com>
2022-08-30 22:43:46 -0300

    add insert no bd

--- src/cap19/TesteConexao.java
+++ src/cap19/TesteConexao.java
@@ -9,23 +9,53 @@ public class TesteConexao {
     static String usuario = "root";
     static String senha = "Imp@ct@";
 
-    public static void abrirDB()  {
+    public static void abrirDB() throws SQLException {
 
+        Connection conexao = null;
         try {
-        String query = "select * from impacta.aluno";
-        Connection conexao =
-                DriverManager.getConnection(url, usuario, senha);
 
-        Statement statement = conexao.createStatement();
-        ResultSet resultSet = statement.executeQuery(query);
+            conexao = DriverManager.getConnection(url, usuario, senha);
 
-        }catch (SQLException e) {
+            //Inserir no BD
+            String queryInsert = "insert into aluno values (?,?,?,?)";
+            PreparedStatement preparedStatement = conexao.prepareStatement(queryInsert);
+
+            // na tabela a ordem da coluna 1 é o campo idaluno
+            preparedStatement.setInt(1, 5);
+
+            // na tabela a ordem da coluna 2 é o campo nome
+            preparedStatement.setString(2, "Inserido pelo jdbc");
+
+            // na tabela a ordem da coluna 3 é o campo nome
+            preparedStatement.setString(3, "23658745996");
+
+            // na tabela a ordem da coluna 3 é o campo nome
+            preparedStatement.setInt(4, 127);
+
+            preparedStatement.executeUpdate();
+
+            // consulta ao BD
+            String querySelect = "select * from impacta.aluno";
+            Statement statement = conexao.createStatement();
+            ResultSet resultSet = statement.executeQuery(querySelect);
+            while (resultSet.next()) {
+                System.out.print("id: " + resultSet.getInt("idaluno"));
+                System.out.print(" nome: " + resultSet.getString("nome"));
+                System.out.print(" cpf: " + resultSet.getString("cpf"));
+                System.out.print(" matricula: " + resultSet.getInt("matricula"));
+                System.out.println();
+            }
+
+        } catch (SQLException e) {
             System.out.println(e);
+        } finally {
+            if (conexao != null) {
+                conexao.close();
+            }
         }
-        System.out.println("Conexão com sucesso!!!");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         abrirDB();
     }
 }
