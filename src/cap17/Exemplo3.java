27950e5d51e415c5f7947662d589ec177e2bb388
Alex Mota <alexmbcdo@gmail.com>
2022-09-05 20:45:02 -0300

    add + exemplos de threads

---
+++ src/cap17/Exemplo3.java
@@ -0,0 +1,32 @@
+package cap17;
+
+public class Exemplo3 {
+
+    public static void main(String[] args) {
+        Thread thread =
+                new Thread(() -> System.out.println(
+                        "Thread interface funcional"));
+
+        thread.start();
+        outraExecucao();
+
+        ExemploRunnable runnable = new ExemploRunnable();
+        Thread teste = new Thread(runnable);
+        teste.start();
+    }
+
+    public synchronized static void outraExecucao() {
+        Thread thread =
+                new Thread(() -> {
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("outra thread");
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+        thread.start();
+    }
+}
--- src/cap17/ExemploRunnable.java
+++ src/cap17/ExemploRunnable.java
@@ -4,13 +4,15 @@ public class ExemploRunnable implements Runnable {
 
     @Override
     public void run() {
-        try {
-            for (int i = 0; i < 10; i++) {
-                System.out.println("implemenatando Runnable");
-                Thread.sleep(100);
+        synchronized (this) {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("implemenatando Runnable");
+                    Thread.sleep(100);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
 }
